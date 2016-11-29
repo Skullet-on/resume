061dc05a0152885353c992539ad9d22c502913c1
Skullet-on <skullet-on@mail.ru>
2016-11-29 12:22:39 +0300

    added pg to product

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ActiveRecord::Base
   has_attached_file :image, styles: { large: "600x600#", medium: "300x300#", small: "150x150#", thumb: "50x50#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  validates :exp, inclusion: { in: %w(small medium large),
-    message: "%{value} is not a valid size" }, allow_nil: true
+ 
 
 
   has_many :skills
