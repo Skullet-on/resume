63f5d05221a3f61339560ad871855ae3e393594f
Skullet-on <skullet-on@mail.ru>
2016-11-29 15:22:50 +0300

    edited abilities from users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,15 @@ class Ability
       can :update, Skill do |skill|
         skill.user == user
       end
+      can :update, Employ do |employ|
+        employ.user == user
+      end
+      can :update, Education do |education|
+        education.user == user
+      end
+      can :update, Course do |course|
+        course.user == user
+      end
       can :create, Employ
       can :create, Education
       can :create, Course
